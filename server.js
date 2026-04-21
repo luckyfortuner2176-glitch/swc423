19d1c638d6c70a0b87279a87e5664ea1d028bbcc
swc442390-beep <swc442390@gmail.com>
2026-04-22 07:20:03 +0800

    cleaning server.js and working on auth.js

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use('/api', authRoutes);
+
 
 // Prevent caching (important after logout)
 app.use((req, res, next) => {
@@ -232,7 +232,7 @@ app.use(session({
     maxAge: 1000 * 60 * 30
   }
 }));
-
+app.use('/api', authRoutes);
 // ==========================
 // AUTH MIDDLEWARE
 // ==========================
